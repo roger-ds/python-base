7a80532a927fc727f2f95c9aa55a753cfa317eea
Rogerio Rodrigues <rogeriodemoraes@gmail.com>
2022-08-15 09:04:40 -0300

    Implementamos o hello world multilinguas.

--- hello.py
+++ hello.py
@@ -1,3 +1,37 @@
 #! /usr/bin/env python3
+"""
+Hello World Multi Linguas.
 
-print('Hello, World !')
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execucao:
+
+    python3 hello.python3
+    ou
+    ./hello.py
+"""
+__version__ = '0.0.1'
+__author__ = 'Rogerio Rodrigues'
+__license__ = 'Unlicense'
+
+import os
+
+current_language = os.getenv('LANG', 'en_US')[:5]
+
+msg = 'Hello, World !'
+
+if current_language == 'pt_BR':
+    msg = 'Olá Mundo !'
+elif current_language == 'it_IT':
+    msg = 'Ciao, Mondo!'
+
+print(msg)
+print(current_language)
+
