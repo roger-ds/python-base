3d47d9fa7446aea6ad56baaab72dedd84a0fab3e
Rogerio Rodrigues <rogeriodemoraes@gmail.com>
2022-08-15 21:56:08 -0300

    elifs adicionados

--- hello.py
+++ hello.py
@@ -31,7 +31,11 @@ if current_language == 'pt_BR':
     msg = 'Olá Mundo !'
 elif current_language == 'it_IT':
     msg = 'Ciao, Mondo!'
+elif current_language == 'fr_FR':
+    msg = 'Bon ju Mounde!'
 
-print(msg)
+print(msg) 
+
 print(current_language)
 
+#
