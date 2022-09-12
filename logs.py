0f7c60dd31e75659ab3d85d6b73f6f4b55275efc
Rogerio Rodrigues <52413452+roger-ds@users.noreply.github.com>
2022-09-12 18:04:55 +0000

    logs.py fixed

--- logs.py
+++ logs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env/ python3
 
-<<<<<<< HEAD
 import logging  # root logger
 
 # nossa instancia
 log = logging.Logger('logs.py', logging.DEBUG) # mostra bruno against root logger ex: __name__
-=======
+
 import logging
 
 # nossa instancia
 log = logging.Logger('logs.py', logging.DEBUG)
->>>>>>> feature
+
 #level
 ch = logging.StreamHandler()
 
@@ -18,17 +17,15 @@ ch = logging.StreamHandler()
 logging.critical('Erro geral ex: banco de dados sumiu')
 logging.error('Erro que afeta uma unica execucao')
 logging.warning('Aviso que nao causa erro')
-<<<<<<< HEAD
 logging.info('Mensagem geral para usuarios')
 logging.debug('Mensagem pro dev, qe, sysadmin')
 
 print('-' * 50)
-=======
+
 logging.debug('Mensagem pro dev, qe, sysadmin')
 logging.info('Mensagem geral para usuarios')
 
 print('-' * 50) 
->>>>>>> feature
 
 try:
   1 / 0
