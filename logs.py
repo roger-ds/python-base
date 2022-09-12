6f7d28b06111d113c2a94afb4bc3ccb9a6ea07e0
Rogerio Rodrigues <rogeriodemoraes@gmaol.com>
2022-09-12 16:45:42 -0300

    add StreamHandlrer

--- logs.py
+++ logs.py
@@ -2,26 +2,26 @@
 import os
 import logging  # root logger
 
+# BOILERPLATE
+# TODO: Usar funcao
+# TODO: Usar lib externa (loguru)
 log_level = os.getenv('LOG_LEVEL', 'WARNING').upper()
-
-# nossa instancia
-log = logging.Logger('logs', log_level) # mostra bruno against root logger ex: __name__
-#level
+log = logging.Logger('logs', log_level) # mostra logs against root logger ex: __name__
 ch = logging.StreamHandler()
 ch.setLevel(log_level)
-# formataçao
 fmt = logging.Formatter(
   '%(asctime)s %(name)s  %(levelname)s l:%(lineno)d f:%(filename)s %(message)s'
 )
 ch.setFormatter(fmt)
-# destino
 log.addHandler(ch)
 
+"""
 log.critical('Erro geral ex: banco de dados sumiu')
 log.error('Erro que afeta uma unica execucao')
 log.warning('Aviso que nao causa erro')
 log.info('Mensagem geral para usuarios')
 log.debug('Mensagem pro dev, qe, sysadmin')
+"""
 
 print('-' * 50)
 
