db3b2e5383f360d03ec859f54330a1479a4765c6
Rogerio Rodrigues <52413452+roger-ds@users.noreply.github.com>
2022-09-12 14:44:59 +0000

    merge de features - logs.py

--- logs.py
+++ logs.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env/ python3
 
+<<<<<<< HEAD
 import logging  # root logger
 
 # nossa instancia
 log = logging.Logger('logs.py', logging.DEBUG) # mostra bruno against root logger ex: __name__
+=======
+import logging
+
+# nossa instancia
+log = logging.Logger('logs.py', logging.DEBUG)
+>>>>>>> feature
 #level
 ch = logging.StreamHandler()
 
@@ -11,10 +18,17 @@ ch = logging.StreamHandler()
 logging.critical('Erro geral ex: banco de dados sumiu')
 logging.error('Erro que afeta uma unica execucao')
 logging.warning('Aviso que nao causa erro')
+<<<<<<< HEAD
 logging.info('Mensagem geral para usuarios')
 logging.debug('Mensagem pro dev, qe, sysadmin')
 
 print('-' * 50)
+=======
+logging.debug('Mensagem pro dev, qe, sysadmin')
+logging.info('Mensagem geral para usuarios')
+
+print('-' * 50) 
+>>>>>>> feature
 
 try:
   1 / 0
