f7b96c221da9a9fa7c477fe54ff9e626414143ba
Rogerio Rodrigues <rogeriodemoraes@gmaol.com>
2022-09-12 10:43:54 -0300

    Adicionado arquivo logs.py

---
+++ logs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env/ python3
+
+import logging  # root logger
+
+# nossa instancia
+log = logging.Logger('logs.py', logging.DEBUG) # mostra bruno against root logger ex: __name__
+#level
+ch = logging.StreamHandler()
+
+
+logging.critical('Erro geral ex: banco de dados sumiu')
+logging.error('Erro que afeta uma unica execucao')
+logging.warning('Aviso que nao causa erro')
+logging.info('Mensagem geral para usuarios')
+logging.debug('Mensagem pro dev, qe, sysadmin')
+
+print('-' * 50)
+
+try:
+  1 / 0
+except ZeroDivisionError as e:
+  logging.error('Deu erro %s', str(e))
