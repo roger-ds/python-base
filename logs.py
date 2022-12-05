ceb056acb77a1a8330a5bd0716650b78f950afe3
Rogerio Rodrigues <rogeriodemoraes@gmail.com>
2022-12-05 07:07:13 -0300

    logs corrigidos

--- logs.py
+++ logs.py
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python3
-
-import logging
-
-# nossa instancia
-log = logging.Logger('logs.py', logging.DEBUG)
-# level
-ch = logging.StreamHandler()    # ch - console handler
-ch.setLevel(logging.DEBUG)
-# formatacao
-fmt = logging.Formatter(
-    '%(asctime)s %(name)s %(levelname) '
-    'l:%$(lineno)d f:%(filename)s: %(message)'
-)
-
-logging.critical('Deu problema geral')
-logging.error('Erro que afera uma unica execucao')
-logging.warning('Aviso que nao causa erro')
-logging.info('Mensagem geral para usuarios')
-logging.debug('Mensagem pro dev, qe, sysadmin')
-
-
-try:
-    1 / 0
-except ZeroDivisionError as e:
-    logging.error('[ERRO] Deu erro %s', str(e))
-
-=======
 #!/usr/bin/env/ python3
 import os
 import logging  # root logger
@@ -61,4 +32,3 @@ try:
   1 / 0
 except ZeroDivisionError as e:
   log.error('Deu erro %s', str(e))
->>>>>>> 80104d5a0c9af86044074a353cae7f5b3fd6b767
